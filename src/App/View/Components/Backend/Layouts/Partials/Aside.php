d9bb722daaf84297405748d1c55768c5033d9151
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-07-19 20:43:50 +0600

    navigation dynamic for user dashboard

--- src/App/View/Components/Backend/Layouts/Partials/Aside.php
+++ src/App/View/Components/Backend/Layouts/Partials/Aside.php
@@ -2,6 +2,7 @@
 
 namespace Sazumme\Themeadmin\App\View\Components\Backend\Layouts\Partials;
 
+use App\Models\Navigation;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
@@ -23,16 +24,30 @@ class Aside extends Component
             $this->navigations = [
                 'Dashboard' => ['name' => 'admin.dashboard', 'params' => []],
                 'Announcements' => ['name' => 'admin.announcements.index', 'params' => []],
+                'Navigations' => ['name' => 'admin.navigations.index', 'params' => []],
                 'Visitor Logs' => ['name' => 'admin.visitorlogs.index', 'params' => []],
             ];
         } else {
             $host = request()->getHost();
             $parts = explode('.', $host);
             $subdomain = count($parts) > 2 ? $parts[0] : null;
+
+            $navigations = Navigation::where('subdomain', $subdomain)->get();
+
             $this->navigations = [
                 'Dashboard' => ['name' => 'user.dashboard', 'params' => ['subdomain' => $subdomain]],
-                'Ebooks' => ['name' => 'ebooks.index', 'params' => ['subdomain' => $subdomain]],
             ];
+
+            $dynamicNavs = $navigations->mapWithKeys(function ($nav) use ($subdomain) {
+                return [
+                    $nav->title => [
+                        'name' => $nav->route,
+                        'params' => ['subdomain' => $subdomain],
+                    ]
+                ];
+            })->toArray();
+
+            $this->navigations = array_merge($this->navigations, $dynamicNavs);
         }
     }
 
