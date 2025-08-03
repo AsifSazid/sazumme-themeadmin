9e26fdebbe38920dfdc34b27be5eebf97f64bf20
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-08-03 20:09:04 +0600

    sidebar niye kaj er majhe push

--- src/App/View/Components/Backend/Layouts/Partials/Aside.php
+++ src/App/View/Components/Backend/Layouts/Partials/Aside.php
@@ -18,36 +18,73 @@ class Aside extends Component
     //     $this->navigations = $navigations;
     // }
 
+    public function getAdminSidebarNavigation($subdomain = null)
+    {
+        $query = Navigation::query()
+            ->whereNull('parent_id')
+            ->where('is_active', false);
+
+        $navigations = $query->with(['children' => function ($q) {
+            $q->where('is_active', false);
+        }])->get();
+
+        return $navigations;
+    }
+    public function getUserSidebarNavigation($subdomain)
+    {
+        // $query = Navigation::query()
+        //     ->whereNull('parent_id')
+        //     ->where('is_active', false)
+        //     ->where('subdomain', $subdomain);
+
+        // $navigations = $query->with(['children' => function ($q) {
+        //     $q->where('is_active', false);
+        // }])->get();
+        $navigations = collect([
+            (object)[
+                'title' => 'Dashboard',
+                'route' => 'admin.dashboard',
+                'url' => route('admin.dashboard'),
+                'nav_icon' => 'fas fa-home',
+                'children' => collect([]),
+            ]
+        ]);
+
+        return $navigations;
+    }
+
     public function __construct()
     {
         if (Auth::guard()->name === 'admin') {
-            $this->navigations = [
-                'Dashboard' => ['name' => 'admin.dashboard', 'params' => []],
-                'Announcements' => ['name' => 'admin.announcements.index', 'params' => []],
-                'Navigations' => ['name' => 'admin.navigations.index', 'params' => []],
-                'Visitor Logs' => ['name' => 'admin.visitorlogs.index', 'params' => []],
-            ];
+            $this->navigations = $this->getAdminSidebarNavigation();
+
+            // $this->navigations = [
+            //     'Dashboard' => ['name' => 'admin.dashboard', 'params' => []],
+            //     'Announcements' => ['name' => 'admin.announcements.index', 'params' => []],
+            //     'Ebook' => ['name' => 'admin.ebooks.index', 'params' => []],
+            //     'Navigations' => ['name' => 'admin.navigations.index', 'params' => []],
+            //     'Visitor Logs' => ['name' => 'admin.visitorlogs.index', 'params' => []],
+            // ];
         } else {
+
             $host = request()->getHost();
             $parts = explode('.', $host);
             $subdomain = count($parts) > 2 ? $parts[0] : null;
 
-            $navigations = Navigation::where('subdomain', $subdomain)->get();
+            // $navigations = Navigation::where('subdomain', $subdomain)->get();
 
-            $this->navigations = [
-                'Dashboard' => ['name' => 'user.dashboard', 'params' => ['subdomain' => $subdomain]],
-            ];
 
-            $dynamicNavs = $navigations->mapWithKeys(function ($nav) use ($subdomain) {
-                return [
-                    $nav->title => [
-                        'name' => $nav->route,
-                        'params' => ['subdomain' => $subdomain],
-                    ]
-                ];
-            })->toArray();
+            $this->navigations = $this->getUserSidebarNavigation($subdomain);
 
-            $this->navigations = array_merge($this->navigations, $dynamicNavs);
+            // dd($this->navigations);
+            // $dynamicNavs = $navigations->mapWithKeys(function ($nav) use ($subdomain) {
+            //     return [
+            //         $nav->title => [
+            //             'name' => $nav->route,
+            //             'params' => ['subdomain' => $subdomain],
+            //         ]
+            //     ];
+            // })->toArray();
         }
     }
 
