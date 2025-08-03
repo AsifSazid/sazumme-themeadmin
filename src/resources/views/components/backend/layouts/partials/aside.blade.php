c115b3693d4c0fde43ccae77186666ff26f54fcf
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-08-03 20:49:50 +0600

    sidebar niye kaj er majhe push

--- src/resources/views/components/backend/layouts/partials/aside.blade.php
+++ src/resources/views/components/backend/layouts/partials/aside.blade.php
@@ -1,4 +1,4 @@
-<aside class="hidden md:block flex-shrink-0 w-64 bg-white border-r dark:border-primary-darker dark:bg-darker">
+<aside class="flex-shrink-0 hidden w-64 bg-white border-r dark:border-primary-darker dark:bg-darker md:block">
     <div class="flex flex-col h-full">
         <nav aria-label="Main" class="flex-1 px-2 py-4 space-y-2 overflow-y-hidden hover:overflow-y-auto">
             {{-- <div x-data="{ isActive: true, open: true }">
